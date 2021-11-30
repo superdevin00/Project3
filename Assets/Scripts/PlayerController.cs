065a5ab17af0ae0a6029a1428a4a2e55fbb1eb78
Devin <superdevin00@gmail.com>
2021-11-29 22:13:01 -0600

    Amogus

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float MoveSpeed = 4.0f;
+    [SerializeField] float JumpSpeed = 4.0f;
+    [SerializeField] float TongueDistanceMax = 2.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
