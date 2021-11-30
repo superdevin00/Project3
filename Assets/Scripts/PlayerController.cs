b09a94434a2f8b3a7cc7e1921cfea07c381740d1
Devin <superdevin00@gmail.com>
2021-11-30 16:04:33 -0600

    Set-up state machine & started extend

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 4.0f;
     [SerializeField] float jumpHeight = 4.0f;
     [SerializeField] float gravity = -9.81f;
+
+    [SerializeField] GameObject tongueTip;
+    [SerializeField] float tongueExtendDuration = 1.0f;
     [SerializeField] float tongueDistanceMax = 2.0f;
+    private float elapsedExtendTime;
 
     [SerializeField] LayerMask terrainMask;
     [SerializeField] Transform groundCheck;
@@ -18,35 +22,76 @@ public class PlayerController : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
     
+    enum playerState {normal, extend, grapple, tumble, splat}
+    private playerState currentState;
+
+
+    private void Start()
+    {
+        currentState = playerState.normal;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, terrainMask);
-        float x = Input.GetAxis("Horizontal");
-        //Vector3 move = transform.right * x;
-
-        if (isGrounded && velocity.y <0)
+        switch (currentState)
         {
-            velocity.y = -2f;
-            velocity.x = x * moveSpeed;
+            //Normal Player State
+            case playerState.normal:
+
+                isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, terrainMask);
+                float x = Input.GetAxis("Horizontal");
+
+                //Grounded Movement
+                if (isGrounded && velocity.y < 0)
+                {
+                    velocity.y = -2f;
+                    velocity.x = x * moveSpeed;
+                }
+
+                //Jump
+                if ((Input.GetKeyDown(KeyCode.X) || Input.GetButtonDown("Jump")) && isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+                }
+
+                //Apply Gravity
+                velocity.y += gravity * Time.deltaTime;
+
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+                    elapsedExtendTime = 0;
+                    currentState = playerState.extend;
+                }
+
+                break;
+
+            case playerState.extend:
+
+                break;
+
+            case playerState.grapple:
+
+                break;
+
+            case playerState.tumble:
+
+                break;
+
+            case playerState.splat:
+
+                break;
+
+            default:
+
+                break;
+
         }
-
-        
-        
-        //controller.Move(move * moveSpeed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
- 
     }
+        
     private void FixedUpdate()
     {
+        //Move
         controller.Move(velocity * Time.deltaTime);
     }
 }
