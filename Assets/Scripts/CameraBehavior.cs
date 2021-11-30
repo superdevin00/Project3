0f867a11af42d7ea1437988f799611d4a84983d8
Devin <superdevin00@gmail.com>
2021-11-29 19:33:40 -0600

    Set-up camera script

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    [SerializeField] GameObject ObjectToTrack;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(0, ObjectToTrack.transform.position.y + 2.0f, -10);
+    }
+}
